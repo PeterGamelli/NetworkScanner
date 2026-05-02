b0514a3ec51e2af0fc4185fb4fffd9ba554dd0ab
Peter Gamelli <petergamelli2277@gmail.com>
2026-05-02 19:33:25 -0400

    created function for pinging multiple devices at once

--- network scanner.py
+++ network scanner.py
@@ -1,1 +1,16 @@
 import subprocess
+
+def check_ip_addr(addr_lst): 
+    results = []
+    for addr in addr_lst: 
+        
+        status, _ = subprocess.getstatusoutput(f'ping /n 1 {addr}')                     
+        if status == 0: 
+            results.append('yes')
+        else: 
+            results.append('no')
+    return results
+
+n = check_ip_addr(input('Please enter IP addresses separated by commas: ').split(','))
+
+print(n)
